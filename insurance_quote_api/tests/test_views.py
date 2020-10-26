0a8947d0437edde73124311fdc1d687e2bf3f494
Patricia Camiansky <patcky@hotmail.com>
2020-10-26 12:19:16 -0300

    refactored tests, still incomplete

---
+++ insurance_quote_api/tests/test_views.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+
+from insurance_quote_api.views import *
+
+class TestInsuranceQuoteViews(TestCase):
+    @classmethod
+    def setUp(self):
+        # generate data sample that will be used in other methods
+        pass
+
+    def test_make_post_call_with_empty_data(self):
+        pass
+
+    def test_make_post_call_with_complete_data(self):
+        pass
+
+    def test_make_post_call_with_incomplete_data(self):
+        pass
+
+    def test_make_get_call(self):
+        pass
--- insurance_quote_api/views.py
+++ insurance_quote_api/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-# from rest_framework.authentication import TokenAuthentication
 
 from dacite import Config, from_dict
 from enum import Enum
