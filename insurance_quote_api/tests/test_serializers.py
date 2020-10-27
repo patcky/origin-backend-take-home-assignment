e69ec5c033e93f8b2a2a810fb102ae542567c2df
Patricia Camiansky <patcky@hotmail.com>
2020-10-27 09:53:48 -0300

    added more tests

--- insurance_quote_api/tests/test_serializers.py
+++ insurance_quote_api/tests/test_serializers.py
@@ -5,7 +5,7 @@ from insurance_quote_api.serializers import QuoteSerializer
 class TestInsuranceQuoteSerializers(TestCase):
     @classmethod
     def setUp(self):
-        # generate data sample that will be used in other methods
+        """Generates data sample that will be used in other methods"""
         self.user_data = {
             "age": 35,
             "dependents": 2,
@@ -17,25 +17,25 @@ class TestInsuranceQuoteSerializers(TestCase):
         }
 
     def test_validate_empty_data(self):
-        # try passing empty data and checking if the serializer is valid
+        """try passing empty data and checking if the serializer is valid"""
         self.user_data = {}
         serializer = QuoteSerializer(data=self.user_data)
         assert serializer.is_valid() == False
 
     def test_validate_incomplete_data(self):
-        # try passing incomplete data and checking if the serializer returns an error with the missing fields
+        """try passing incomplete data and checking if the serializer is valid"""
         del self.user_data["age"]
         del self.user_data["dependents"]
         serializer = QuoteSerializer(data=self.user_data)
         assert serializer.is_valid() == False
 
     def test_validate_complete_data(self):
-        # try passing data with complete data and checking if the serializer validates the fields
+        """try passing complete data and checking if the serializer is valid"""
         serializer = QuoteSerializer(data=self.user_data)
         assert serializer.is_valid() == True
 
     def test_validate_wrong_format_data(self):
-        # try passing wrong format data and checking if the serializer returns an error
+        """try passing wrong format data and checking if the serializer is valid"""
         self.user_data["dependents"] = "wrong format"
         serializer = QuoteSerializer(data=self.user_data)
         assert serializer.is_valid() == False
--- insurance_quote_api/tests/test_views.py
+++ insurance_quote_api/tests/test_views.py
@@ -1,17 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 from insurance_quote_api.views import *
 
 class TestInsuranceQuoteViews(TestCase):
-    # These are e2e tests
     @classmethod
     def setUp(self):
-        # generate data sample that will be used in other methods
-        pass
+        """Generates data sample that will be used in other methods"""
+        self.user_data = {
+            "age": 35,
+            "dependents": 2,
+            "house": {"ownership_status": "owned"},
+            "income": 0,
+            "marital_status": "married",
+            "risk_questions": [0, 1, 0],
+            "vehicle": {"year": 2018}
+        }
+        self.client = Client()
 
     def test_make_post_call_with_empty_data(self):
         # try making a post call with empty data and checking if the view returns errors
-        pass
+        self.user_data = {}
 
     def test_make_post_call_with_complete_data(self):
         # try making a post call with complete data and checking if the view returns complete result
@@ -19,7 +27,8 @@ class TestInsuranceQuoteViews(TestCase):
 
     def test_make_post_call_with_incomplete_data(self):
         # try making a post call with incomplete data and checking if the view returns errors
-        pass
+        del self.user_data["age"]
+        del self.user_data["dependents"]
 
     def test_make_get_call(self):
         # try making a get call and checking if the view returns HTTP method not allowed
